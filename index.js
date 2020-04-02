f39cc1c0911000ce9677d0528ad3dce82e1f6ffd
Dimitrije Mitic <dimitrije.mitic@oasisdigital.com>
2020-04-02 16:30:53 -0400

    added dynamic chart

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let requestOptions = {
     };
 let dataArray = [];            //holds parsed data for each country
 let globalTotals = [0, 0, 0]; // holds the global number of cases, deaths, and recovered
+let allThisData = [];
+let chartMap = new Map();
 
 let refreshPage = setInterval(getData, 1000000); // refreshes api call every 1M seconds
 
@@ -19,13 +21,13 @@ function getData() {
         return res.json();
     }).then(obj => {
         globalTotals = [0, 0, 0];
-        console.log(obj);
         idx = 0;
         for (let [key, value] of Object.entries(obj.Countries)) {
             if(!value.Country == '' && !value.Country.includes('The') && !value.Country.includes('Republic ') && !value.Country.includes('(Islamic') && !value.Country.includes('*') && !value.Country.includes('Nam') && !value.Country.includes('SAR') && !value.Country.includes('Russian F') && !value.Country.includes('St.')) {
                 let newEntry = {
                     id: idx = 0,
                     country: value.Country == 'US' ? 'United States' : (value.Country == 'Korea, South' ? 'South Korea' : value.Country),
+                    slug: value.Slug,
                     totalconfirmed: value.TotalConfirmed,
                     totaldeaths: value.TotalDeaths,
                     totalrecovered: value.TotalRecovered
@@ -53,16 +55,11 @@ function getData() {
 // bottom box relevant to the country selected.
 //****************************************************************************************
 function getDataForCountry(selectedIndex) {
-    if(dataArray[selectedIndex]?.totalconfirmed < 1) {
-        document.getElementById('currentcountry').setAttribute("style", "color: #00c767");
-    }
-    else {
-        document.getElementById('currentcountry').setAttribute("style", "color: #E800000");
-    }
     document.getElementById('currentcountry').innerText = dataArray[selectedIndex]?.country;
     document.getElementById('currentcases').innerText = "Cases: " + numFormat(dataArray[selectedIndex]?.totalconfirmed);
     document.getElementById('currentdeaths').innerText = "Deaths: " + numFormat(dataArray[selectedIndex]?.totaldeaths);
     document.getElementById('currentrecovered').innerText = "Recovered: " + numFormat(dataArray[selectedIndex]?.totalrecovered);
+    getDailyByCountry(dataArray[selectedIndex]);
 }
 
 //****************************************************************************************
@@ -90,4 +87,76 @@ function numFormat(num) {
     }
 
     return s.split("").reverse().join("") || "0";
+}
+
+function getDailyByCountry(country) {
+    document.getElementById('graph').style.display = "none";
+    document.getElementById('loading').style.display = "block";
+    chartMap.clear();
+    fetch("https://api.covid19api.com/country/" + country.slug +"/status/confirmed", requestOptions)
+  .then(response => response.json())
+  .then(result => allThisData = result)
+  .then(_=> {
+      let date;
+    for(let o of allThisData) {
+        if(o.Status == "confirmed") {
+            date = new Date(o.Date).getTime();
+            chartMap.set(date, (chartMap.get(date) || 0) + o.Cases); 
+            console.log(chartMap.get(date) + " cases of COVID in " + country.country);
+        }
+    }
+    makeChart(chartMap);
+  })
+  .catch(error => console.log('error', error));
+}
+
+
+function makeChart(map) {
+    document.getElementById('graph').style.display = "block";
+    document.getElementById('loading').style.display = "none";
+    const ctx = document.getElementById('graph').getContext('2d');
+    let xlabels = [];
+    xlabels = Array.from(map.keys());
+    for(let i = 0; i < xlabels.length; i++) {
+        xlabels[i] = new Date(xlabels[i]).toString().substring(4, 10);
+    }
+    let ylabels = [];
+    ylabels = Array.from(map.values());
+    
+    const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: xlabels,
+        datasets: [{
+            label: 'COVID-19 Cases',
+            data: ylabels,
+            pointRadius: 2,
+            backgroundColor: 
+                'rgba(232, 0, 0, 0.15)',
+            borderColor: 
+                'rgba(232, 0, 0, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            xAxes :[{
+                ticks: {fontSize: 10}
+            }],
+            yAxes: [{
+                ticks: {
+                    fontSize: 10,
+                    beginAtZero: true,
+                    callback: function(value, index, values) {
+                        if(Math.floor(value / 1000) > 1) {
+                            return value/1000 + 'k';
+                        } else {
+                            return value;
+                        }
+                    }
+                }
+            }]
+        }
+    }
+});
 }
